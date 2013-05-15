5402d0373b137007105441406c19133b045fc664
Augusto Ribas <ribas.aca@gmail.com>
2013-05-15 00:53:36 -0400

    Esperando resultado do BOCA

--- pratica/listaBOCA2/fracaominima.c
+++ pratica/listaBOCA2/fracaominima.c
@@ -2,31 +2,33 @@
 
 int main(void)
 {
+  int n , i , contador , numerador , denominador , divisor , maxdiv;
 
   scanf("%d", &n);
   contador= 1;
 
-  while(i<=n) {
-    scanf("%d %d", &numerador , &denominador)
-      
+  while(contador<=n) {
+    scanf("%d %d", &numerador , &denominador);
+
       if(numerador<denominador)
-	divisor = numerador ;
+	divisor = numerador;
       else
-	divisor = denominador ;
+	divisor = denominador;
+
+    maxdiv = 1;
 
     for(i=2 ; i<=divisor ; i++) {
-
-      if(numerador % i == 0 && denominador % i == 0 )
-	maxdiv=i;
+      if(numerador % i == 0 && denominador % i == 0 ) {
+	maxdiv = i;
+      }
     }
-   
-    printf("%d %d\n",numerador/maxdiv,denominador/maxdiv);
 
+    printf("%d %d\n",numerador /  maxdiv,  denominador / maxdiv);
+
+    contador++;
 
   }
 
-
-
-
   return 0;
+
 }
