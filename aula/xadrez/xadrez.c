8b59b26fe29f508b042bb42fec4a48b32bc60476
Augusto Ribas <ribas.aca@gmail.com>
2013-07-10 11:08:40 -0400

    mexendo no xadrez.c

--- aula/xadrez/xadrez.c
+++ aula/xadrez/xadrez.c
@@ -3,29 +3,69 @@
 typedef struct{
   char peca;
   int linha , coluna;
-} registro;
+} peca;
+
+
+/*Testa se é check, por enquanto so funciona com o peão, ai a ideia é ir adicionando peças*/
+int xeque(peca rei , peca adversario , char cor) {
+
+
+  /*testando o peão*/
+  if(cor=='b' && adversario.peca=='P') {
+    if(  ((rei.linha-1==adversario.linha-1) && (rei.coluna-1==adversario.coluna-1 ))  ||
+	  ((rei.linha+1==adversario.linha+1) && (rei.coluna-1==adversario.coluna-1))  )
+      return 1;
+    else
+      return 0;
+  } else {
+    if( ((rei.linha-1==adversario.linha-1) && (rei.coluna+1==adversario.coluna+1))  ||
+        ((rei.linha+1==adversario.linha+1) && (rei.coluna+1==adversario.coluna+1)) )
+      return 1;
+    else
+      return 0;
+  }
+  /*fim do teste do peão*/
+
+  return 666;
+}
+
+/*aqui eu so estou fazendo uma função pra fazer uma matriz, porque pra torre eu tenho que testar se nao tem nada
+  no caminho correto? Futuramente o tabuleiro vai em junto pro teste xeque*/
+void tabu(peca ataca[], int a, peca defende[] , int d ,peca remove, int tabuleiro[8][8]) {
+  int i;
+  for(i=0;i<a;i++) {
+    tabuleiro[ataca[i].linha][ataca[i].coluna]=1;
+  }
+  for(i=0;i<d;i++) {
+    tabuleiro[defende[i].linha][defende[i].coluna]=1;
+  }
+
+  tabuleiro[remove.linha][remove.coluna]=0;
+}
+
 
 
 int main(void)
 {
-  registro peca[2][16],  movimento[2];
+  peca branca[16], preta[16],  movimento[2];
   char coluna[2];
-  int b,p,i,linha[2], tabuleiro[8][8]={{0}};
+  int b,p,i,j,linha[2], tabuleiro[8][8]={{0}};
+
 
   scanf("%d",&b);
 
   for(i=0;i<b;i++) {
-    scanf("\n %c %d%c",&peca[0][i].peca,&linha[0],&coluna[0]);
-    peca[0][i].linha=linha[0]-1;
-    peca[0][i].coluna=coluna[0]-97;
+    scanf("\n %c %d%c",&branca[i].peca,&linha[0],&coluna[0]);
+    branca[i].linha=linha[0]-1;
+   branca[i].coluna=coluna[0]-97;
   }
 
   scanf("%d",&p);
 
   for(i=0;i<p;i++) {
-    scanf("\n %c %d%c",&peca[1][i].peca,&linha[0],&coluna[0]);
-    peca[1][i].linha=linha[0]-1;
-    peca[1][i].coluna=coluna[0]-97;
+    scanf("\n %c %d%c",&preta[i].peca,&linha[0],&coluna[0]);
+    preta[i].linha=linha[0]-1;
+    preta[i].coluna=coluna[0]-97;
   }
 
   scanf("\n %c %d%c %d%c",&movimento[0].peca,&linha[0],&coluna[0],&linha[1],&coluna[1]);
@@ -35,21 +75,24 @@ int main(void)
   movimento[1].linha=linha[1]-1;
   movimento[1].coluna=coluna[1]-97;
 
-  for(i=0;i<b;i++) {
-    tabuleiro[peca[0][i].linha][peca[0][i].coluna]=peca[0][i].peca;
+
+  tabu(branca,b, preta , p ,preta[0], tabuleiro);
+
+  /*Aqui é so um teste pra ver se ta preenchendo certo a matriz*/
+  for(i=0;i<8;i++) {
+    for(j=0;j<8;j++) {
+      printf("%3d ",tabuleiro[i][j]);
+    }
+    printf("\n");
   }
 
-  for(i=0;i<p;i++) {
-    tabuleiro[peca[1][i].linha][peca[1][i].coluna]=peca[1][i].peca;
-  }
+  /*Aqui é o uso do teste em si, 0 para não esta em xeque e 1 para esta em xeque*/
+  printf("%d\n",xeque(branca[1],preta[0],'b'));
 
-  for(i=0;i<8;i++)
-    for(j=0
 
-  printf("fim\n");
-
-  for(i=0;i<b;i++)
-  printf("Peça: %c linha: %d coluna: %d\n",peca[0][i].peca,peca[0][i].linha,peca[0][i].coluna);
 
   return 0;
 }
+
+
+
