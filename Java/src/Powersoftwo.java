abaf4604ab94f74d39414210c21c8b0d9ea51e59
Augusto Ribas <ribas.aca@gmail.com>
2013-12-16 12:01:19 -0300

    Aprendendo Java basico

---
+++ Java/src/Powersoftwo.java
@@ -0,0 +1,16 @@
+
+public class Powersoftwo {
+
+	public static void main(String[] args) {
+		int N = Integer.parseInt(args[0]);
+		int v=1;
+		int i=0;
+		while(i<=N) {
+			System.out.println(i + " " + v);
+			v=2*v;
+			i=i+1;
+		}
+
+	}
+
+}
