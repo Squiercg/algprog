fae2af3df87e0057b370511ed130a3fb187167d0
Augusto Ribas <ribas.aca@gmail.com>
2014-03-21 14:12:34 -0400

    Atrasado nos algoritimos

--- Estrutura de dados/Arvore Binaria/arvore.c
+++ Estrutura de dados/Arvore Binaria/arvore.c
@@ -8,6 +8,112 @@ typedef struct Arvore{
   struct Arvore *direita;
 } arvore;
 
+//void imprime_arvore(arvore *raiz, int altura) {
+//}
+
+
+/*
+ *
+ *Funções primaria de arvore primaria
+ *
+ **/
+
+/*Função de inserção*/
+arvore* arvore_insere(arvore* raiz, int valor);
+/*Função de remoçao*/
+arvore* arvore_remove(arvore* raiz, int valor);
+/*Função de busca*/
+arvore* busca_arvore(arvore* raiz, int n);
+
+/*Função para imprimir a arvore*/
+/*Pre-ordem*/
+void imprime_preordem (arvore* raiz);
+/*simetrica*/
+void imprime_simetrica (arvore* raiz);
+/*pos-ordem*/
+void imprime_posordem (arvore* raiz);
+
+/*
+ *
+ *Funções para arvore avl
+ *
+ **/
+
+int altura(arvore* raiz) {
+
+  int iEsq,iDir;
+
+  if (raiz == NULL) {
+    return 0;
+  }
+
+  iEsq = altura(raiz->esquerda);
+  iDir = altura(raiz->direita);
+
+  if ( iEsq > iDir ) {
+    return iEsq + 1;
+  } else {
+    return iDir + 1;
+  }
+
+}
+
+
+int main(void) {
+
+  int i, n,x;
+
+  arvore *exemplo;
+
+  exemplo=NULL;
+
+  printf("Entre com o tamanho da árvore: ");
+  scanf("%d",&n);
+
+  for(i=0;i<n;i++) {
+    exemplo=arvore_insere(exemplo,rand()%101);
+  }
+
+  printf("\nPre-ordem\n");
+  imprime_preordem (exemplo);
+
+  printf("\nsimetrica\n");
+  imprime_simetrica (exemplo);
+
+  printf("\npos-ordem\n");
+  imprime_posordem (exemplo);
+
+  printf("\n");
+  printf("Entre com um numero para buscar na arvore: ");
+  scanf("%d",&x);
+
+  printf("Pondeiro: %p\n",(void*)busca_arvore(exemplo,x));
+
+  printf("Entre com um numero remover da arvore na arvore: ");
+  scanf("%d",&x);
+
+  printf("\nAntes da remoção\n");
+  imprime_simetrica (exemplo);
+
+  arvore_remove(exemplo,x);
+
+  printf("\nDepois da remoção\n");
+  imprime_simetrica (exemplo);
+
+  printf("\n");
+
+  printf("A altura atual dessa arvore é %d\n",altura(exemplo));
+
+
+  //  print_tree(exemplo, altura(exemplo));
+
+
+
+  printf("\n");
+  return 0;
+}
+
+
 /*Função de inserção*/
 arvore *arvore_insere (arvore* raiz, int valor) {
 
@@ -27,19 +133,73 @@ arvore *arvore_insere (arvore* raiz, int valor) {
 
 }
 
-/*Função de busca*/
-/*
-arvore* (arvore* int v) {
+/*Função de remoçao*/
+arvore* arvore_remove (arvore* raiz, int valor) {
 
-  if (r == NULL)
+  if (raiz == NULL) {
     return NULL;
-  else if (r->info > v)
-    return abb_busca (r->esq, v);
-  else if (r->info < v)
-    return abb_busca (r->dir, v);
-  else return r;
+  } else {
+    if (raiz->chave > valor) {
+      raiz->esquerda = arvore_remove(raiz->esquerda, valor);
+    } else {
+      if (raiz->chave < valor) {
+	raiz->direita = arvore_remove(raiz->direita, valor);
+      }	else {
+
+/* Achamos o nó a remover*/
+/* Nó sem filhos */
+	if (raiz->esquerda == NULL && raiz->direita == NULL) {
+	  free (raiz);
+	  raiz = NULL;
+	} else {
+
+/* nó só tem filho à direita */
+	  if (raiz->esquerda == NULL) {
+	    arvore* temporario = raiz;
+	    raiz = raiz->direita;
+	    free (temporario);
+	  } else {
+/* só tem filho à esquerda */
+	    if (raiz->direita == NULL) {
+	      arvore* temporario = raiz;
+	      raiz = raiz->esquerda;
+	      free (temporario);
+	    } else {
+/* nó tem os dois filhos */
+	      arvore* f = raiz->esquerda;
+	      while (f->direita != NULL) {
+		f = f->direita;
+	      }
+	      raiz->chave = f->chave; /* troca as informações */
+	      f->chave = valor;
+	      raiz->esquerda = arvore_remove(raiz->esquerda,valor);
+	    }
+	  }
+	}
+      }
+    }
+  }
+  return raiz;
 }
-*/
+
+/*Função de busca*/
+arvore* busca_arvore(arvore* raiz, int n) {
+
+  if (raiz == NULL) {
+    return NULL;
+  } else {
+
+    if (raiz->chave > n) {
+      return busca_arvore (raiz->esquerda, n);
+    } else {
+      if (raiz->chave < n) {
+	return busca_arvore (raiz->direita, n);
+      } else {
+	return raiz;
+      }
+    }
+  }
+}
 
 /*Função para imprimir a arvore*/
 /*Pre-ordem*/
@@ -65,33 +225,4 @@ void imprime_posordem (arvore* raiz) {
     imprime_posordem(raiz->direita);
     printf("%d ",raiz->chave);
   }
-}
-
-int main(void) {
-
-  int i, n;
-
-  arvore *exemplo;
-
-  exemplo=NULL;
-
-  printf("Entre com o tamanho da árvore: ");
-  scanf("%d",&n);
-
-  for(i=0;i<n;i++) {
-    exemplo=arvore_insere(exemplo,rand()%101);
-  }
-
-  printf("\nPre-ordem\n");
-  imprime_preordem (exemplo);
-
-  printf("\nsimetrica\n");
-  imprime_simetrica (exemplo);
-
-  printf("\npos-ordem\n");
-  imprime_posordem (exemplo);
-
-
-  printf("\n");
-  return 0;
 }
